d7d3e0e38b31b15901046f6c8b9becff1915373d
marcjofreProf <119196997+marcjofreProf@users.noreply.github.com>
2026-09-11 14:27:42 +0200

    Refactor phases in SDN protocol switching test script

--- tests/test-sdn-protocol-switching-performance.sh
+++ tests/test-sdn-protocol-switching-performance.sh
@@ -58,30 +58,40 @@ run_lifecycle_benchmark() {
     for ((i=1; i<=ITERATIONS; i++)); do
         echo -n "[*] Iteration ${i}/${ITERATIONS}... "
 
-        # 1. Connect
+        # Connect Phase
         if [ "$nb_proto" == "RESTCONF" ]; then
             t_conn=$(time_exec "curl -s -f -X POST '${RESTCONF_GW_URL}' -H 'Content-Type: application/json' -H 'X-Southbound-Target: ${sb_proto}' -d '{\"service-id\":\"qservice-m${mode_id}\",\"target-node-ip\":\"${TARGET_NODE_IP}\",\"ingress-port\":1,\"egress-port\":2,\"admin-state\":\"ENABLED\"}'")
+        elif [ "$sb_proto" == "NETCONF" ]; then
+            t_conn=$(time_exec "kubectl exec -n micro-onos sdn-adapter -- python3 /tmp/netconf-switching-client.py ${TARGET_NODE_IP} connect")
+        elif [ "$sb_proto" == "gNOI" ]; then
+            t_conn=$(time_exec "kubectl exec -n micro-onos sdn-adapter -- python3 -c \"import socket; socket.create_connection(('${TARGET_NODE_IP}', 50051), timeout=2)\"")
         else
             t_conn=$(time_exec "gnmic -a ${ONOS_GNMI_TARGET} --tls-cert /etc/onos/certs/tls.crt --tls-key /etc/onos/certs/tls.key --skip-verify --target ${TARGET_DEVICE} set --update '/interfaces/interface[name=eth1]/config/name:::string:::eth1' --update '/interfaces/interface[name=eth1]/config/description:::string:::qservice-m${mode_id}-${sb_proto}'")
         fi
-
-        # 2. Status 1
+        
+        # Status 1 Phase
         if [ "$nb_proto" == "RESTCONF" ]; then
             t_stat1=$(time_exec "curl -s -f -X GET '${RESTCONF_GW_URL}?sb=${sb_proto}'")
+        elif [ "$sb_proto" == "NETCONF" ]; then
+            t_stat1=$(time_exec "kubectl exec -n micro-onos sdn-adapter -- python3 /tmp/netconf-switching-client.py ${TARGET_NODE_IP} status")
         else
             t_stat1=$(time_exec "gnmic -a ${ONOS_GNMI_TARGET} --tls-cert /etc/onos/certs/tls.crt --tls-key /etc/onos/certs/tls.key --skip-verify --target ${TARGET_DEVICE} get --path '/interfaces/interface[name=eth1]'")
         fi
-
-        # 3. Disconnect
+        
+        # Disconnect Phase
         if [ "$nb_proto" == "RESTCONF" ]; then
             t_disc=$(time_exec "curl -s -f -X DELETE '${RESTCONF_GW_URL}?service-id=qservice-m${mode_id}&sb=${sb_proto}'")
+        elif [ "$sb_proto" == "NETCONF" ]; then
+            t_disc=$(time_exec "kubectl exec -n micro-onos sdn-adapter -- python3 /tmp/netconf-switching-client.py ${TARGET_NODE_IP} disconnect")
         else
             t_disc=$(time_exec "gnmic -a ${ONOS_GNMI_TARGET} --tls-cert /etc/onos/certs/tls.crt --tls-key /etc/onos/certs/tls.key --skip-verify --target ${TARGET_DEVICE} set --delete '/interfaces/interface[name=eth1]/config/description'")
         fi
-
-        # 4. Status 2
+        
+        # Status 2 Phase
         if [ "$nb_proto" == "RESTCONF" ]; then
             t_stat2=$(time_exec "curl -s -X GET '${RESTCONF_GW_URL}?sb=${sb_proto}'")
+        elif [ "$sb_proto" == "NETCONF" ]; then
+            t_stat2=$(time_exec "kubectl exec -n micro-onos sdn-adapter -- python3 /tmp/netconf-switching-client.py ${TARGET_NODE_IP} status")
         else
             t_stat2=$(time_exec "gnmic -a ${ONOS_GNMI_TARGET} --tls-cert /etc/onos/certs/tls.crt --tls-key /etc/onos/certs/tls.key --skip-verify --target ${TARGET_DEVICE} get --path '/interfaces/interface[name=eth1]'")
         fi
